ccd7df12ae839f449fd524e5be79d9ebc89e0223
elisemercury <elisejlandman@hotmail.com>
2025-10-19 10:59:52 +0100

    Convert to static

---
+++ script.js
@@ -0,0 +1,9 @@
+// Handle logo click - redirect to main website
+function handleLogoClick() {
+    window.location.href = 'https://www.difpy.app';
+  }
+  
+  // Handle button clicks - open URLs in new tab
+  function handleRedirect(url) {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
